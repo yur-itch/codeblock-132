d79d196ddd78271085f5e6ed11fbfe11cbeec312
bcgtrunner <ilya.timakov24@gmail.com>
2026-02-10 17:36:43 +0700

    function done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+var a = 0;
+
+init();
+
+
+Print = function () {
+    console.log("Hello, World!");
+    console.log(a);
+}
+Print();
+
+function init() {
+    a += 30;
+}
